aaed92d441be29eb2c462775ce25874911174d4e
görkem serin <gorkemserin@outlook.com>
2024-03-18 16:21:07 +0300

    Initial commit

--- index.js
+++ index.js
@@ -27,15 +27,13 @@ app.get("/", async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM items ORDER BY id ASC") 
     items = result.rows;
-    res.render("index.ejs", {
-      listTitle: "Today",
-      listItems: items,
-    });
   } catch (error) {
     console.error(error);
   }
-
-
+  res.render("index.ejs", {
+    listTitle: "Today",
+    listItems: items,
+  });
 });
 
 app.post("/add", async (req, res) => {
